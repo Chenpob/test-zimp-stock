a58778b7a6dd070a0fe0f7c2e6de3d6cf020cf01
Chenpob Nopakiat <jienpowg@gmail.com>
2023-04-12 22:21:11 +0700

    Add cache data  with node-cache

--- routes/stocks.js
+++ routes/stocks.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('isomorphic-fetch')
+const NodeCache = require('node-cache')
+
+const cache = new NodeCache({ stdTTL: 60 });
+
 
 router.get('/:symbol', async (req, res) => {
+  
   const symbol = req.params.symbol.toLowerCase();
+
+  const cachedData = cache.get(symbol);
+  if (cachedData) {
+    return res.json({ symbol: cachedData.symbol, price: cachedData.price });
+  }
+
   const modules = 'financialData';
   const response = await fetch(`https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=${modules}`);
   const text = await response.text();
   const match = text.match(/"currentPrice":{"raw":(\d+\.\d+),"fmt":"(\d+\.\d+)"/);
+  
   if (match) {
     const price = parseFloat(match[1]);
+    cache.set(symbol, { symbol, price });
     res.json({ symbol, price });
   } else {
     res.status(404).json({ error: `Not found for ${symbol}` });
