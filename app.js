917744f494aeb77fccc48014a7377a8fda969dbb
Chenpob Nopakiat <jienpowg@gmail.com>
2023-04-12 21:03:13 +0700

    inatall isomorphic-fetch and create stock router api get for get current price with symbol

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const stocksRouter = require('./routes/stocks');
 
 const app = express();
 
@@ -21,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/stocks', stocksRouter);
 
-app.listen(8000, () => console.log('Server started on port 8000'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/stocks.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('isomorphic-fetch')
+
+router.get('/:symbol', async (req, res) => {
+  const symbol = req.params.symbol.toLowerCase();
+  const modules = req.query.modules.toLocaleLowerCase();
+  const response = await fetch(`https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=${modules}`);
+  const text = await response.text();
+  const match = text.match(/"currentPrice":{"raw":(\d+\.\d+),"fmt":"(\d+\.\d+)"/);
+  if (match) {
+    const price = parseFloat(match[1]);
+    res.json({ symbol, price });
+  } else {
+    res.status(404).json({ error: `Not found for ${symbol}` });
+  }
+});
+
+
+module.exports = router;
